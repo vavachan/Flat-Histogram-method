7050854730236ddb27d83703705a517dd02aa170
vavachan <varghese.i.am@gmail.com>
2016-04-21 16:26:43 +0530

    entropy_1para_fit

--- cc/fitting_scipy.py
+++ cc/fitting_scipy.py
@@ -3,13 +3,20 @@ import scipy
 from scipy import optimize
 import math
 import sys
-m=float(sys.argv[1])
+from scipy import interpolate
+import pylab as pl
 def func(x,a,b,c):
 	return a*x**2+b*x+c
-exec "M=np.loadtxt('m_%d.dat')" %m
-A=M[:,2]
-B=M[:,0]
-popt,pcov = optimize.curve_fit(func,B,A)
-print len(A)
-for i in range(0,len(A)):
-	print B[i],A[i],func(B[i],popt[0],popt[1],popt[2])
+exec "M=np.loadtxt('entropy_1para_6.dat')" 
+N=6**3
+A=M[:,1]/N
+B=M[:,0]/N
+tck = interpolate.splrep(B,A,s=0)
+xnew=np.arange(0,3*8**3,2)
+xnew=xnew/(8.0**3)
+ynew = interpolate.splev(xnew, tck, der=0)
+pl.plot(xnew,ynew)
+pl.plot(B,A)
+pl.show()
+np.savetxt('entropy_1para_ini.dat', np.c_[xnew*(8**3),ynew])
+
